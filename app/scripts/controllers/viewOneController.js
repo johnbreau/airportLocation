b948618379868d8dc3cb28842b2b5fe8615b0a0c
John Breau <john.breau@vml.com>
2016-05-24 15:45:44 -0500

    built array from response object

--- app/scripts/controllers/viewOneController.js
+++ app/scripts/controllers/viewOneController.js
@@ -4,26 +4,10 @@ angular.module('airportLocation')
   function viewOneController($scope, $http) {
 
     $http.get("http://mysafeinfo.com/api/data?list=englishmonarchs&format=json", true).success(function(response){
-        $scope.responses = response;
+      $scope.responses = response;
+      let myArray = Object.keys($scope.responses).map(key => $scope.responses[key]);
+      console.log(myArray[0]);
     })
-
-  var promise = new Promise(function(resolve, reject) {
-
-    var me = false;
-
-    if (me === false) {
-      resolve("Stuff worked!");
-    }
-    else {
-      reject(Error("It broke"));
-    }
-  });
-
-  promise.then(function(result) {
-    console.log(result); // "Stuff worked!"
-    }, function(err) {
-      console.log(err); // Error: "It broke"
-    });
 
     var myEl = angular.element(document.getElementsByClassName('clickerClass'));
 
@@ -34,3 +18,21 @@ angular.module('airportLocation')
   };
 
 // http://api.census.gov/data/2014/acs1/variables.json
+
+// var promise = new Promise(function(resolve, reject) {
+//
+// var me = false;
+//
+// if (me === false) {
+//   resolve("Stuff worked!");
+// }
+// else {
+//   reject(Error("It broke"));
+// }
+// });
+//
+// promise.then(function(result) {
+// console.log(result); // "Stuff worked!"
+// }, function(err) {
+//   console.log(err); // Error: "It broke"
+// });
