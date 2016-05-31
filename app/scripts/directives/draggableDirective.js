c26f64773cfb99a04e09576eb8fc078e9491939e
John Breau <john.breau@vml.com>
2016-05-31 16:41:05 -0500

    quaggaJS added to package

--- app/scripts/directives/draggableDirective.js
+++ app/scripts/directives/draggableDirective.js
@@ -1,6 +1,8 @@
 angular.module('airportLocation').directive('dragMe', function() {
   return {
     restrict: 'A',
+    controller: 'draggableController',
+    controllerAs: 'dragCtrl',
     link: function(scope, elm, attrs) {
       var options = scope.$eval(attrs.dragMe); //allow options to be passed in
       elm.draggable(options);
