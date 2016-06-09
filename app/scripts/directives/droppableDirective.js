bfeb7b20999d3f22043829634f38ab16b3e1c080
John Breau <john.breau@vml.com>
2016-06-09 09:59:47 -0500

    test commit

--- app/scripts/directives/droppableDirective.js
+++ app/scripts/directives/droppableDirective.js
@@ -10,7 +10,8 @@ angular.module('airportLocation').directive('dropMe', function() {
               dragEl = angular.element(ui.draggable).parent(),
               dropEl = angular.element(this);
 
-          if (dragEl.hasClass('list1') && !dropEl.hasClass('list1') && reject !== true) {
+          if (dragEl.hasClass('clickClass') && reject !== true) {
+            alert('that hit the spot');
             scope.list2.push(scope.list1[dragIndex]);
             scope.list1.splice(dragIndex, 1);
           } else if (dragEl.hasClass('list2') && !dropEl.hasClass('list2') && reject !== true) {
